818a8bca2831b06fcc59b04d2c53a6950b6bbb61
teataeho <rlaxogh9504@gmail.com>
2023-05-15 14:48:14 +0900

    2023.05.15 MyBatis

--- SpringDBAccess/src/main/java/com/spring/db/service/BoardService.java
+++ SpringDBAccess/src/main/java/com/spring/db/service/BoardService.java
@@ -3,41 +3,47 @@ package com.spring.db.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import com.spring.db.model.BoardVO;
 import com.spring.db.repository.IBoardDAO;
+import com.spring.db.repository.IBoardMapper;
 
 @Service
 public class BoardService implements IBoardService {
 	
+//	@Autowired
+//	@Qualifier("boardDAO")
+//	private IBoardDAO dao;
+	
 	@Autowired
-	private IBoardDAO dao;
+	private IBoardMapper mapper;
 
 	@Override
 	public void insertArticle(BoardVO vo) {
 		System.out.println("vo: " + vo);
-		dao.insertArticle(vo);
+		mapper.insertArticle(vo);
 	}
 
 	@Override
 	public List<BoardVO> getArticles() {		
-		return dao.getArticles();
+		return mapper.getArticles();
 	}
 
 	@Override
 	public BoardVO getArticle(int bno) {		
-		return dao.getArticle(bno);
+		return mapper.getArticle(bno);
 	}
 
 	@Override
 	public void deleteArticle(int bno) {
-		dao.deleteArticle(bno);
+		mapper.deleteArticle(bno);
 	}
 
 	@Override
 	public void updateArticle(BoardVO vo) {
-		dao.updateArticle(vo);
+		mapper.updateArticle(vo);
 	}
 
 }
