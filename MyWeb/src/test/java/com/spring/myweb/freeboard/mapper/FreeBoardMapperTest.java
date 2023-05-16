80b7be33bc5ca1b82948f0810184c67a8585929f
teataeho <rlaxogh9504@gmail.com>
2023-05-16 14:55:01 +0900

    2023.05.16 controller test

--- MyWeb/src/test/java/com/spring/myweb/freeboard/mapper/FreeBoardMapperTest.java
+++ MyWeb/src/test/java/com/spring/myweb/freeboard/mapper/FreeBoardMapperTest.java
@@ -2,7 +2,6 @@ package com.spring.myweb.freeboard.mapper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
