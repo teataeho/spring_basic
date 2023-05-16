1d44bbc7a29b2203c0fbb3a1955bd79ab6fd8eb2
teataeho <rlaxogh9504@gmail.com>
2023-05-16 18:25:35 +0900

    2023.05.16

--- MyWeb/src/main/java/com/spring/myweb/FreeBoardController.java
+++ MyWeb/src/main/java/com/spring/myweb/FreeBoardController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,24 +25,37 @@ public class FreeBoardController {
 		model.addAttribute("boardList", service.getList());
 	}
 	
+	//글쓰기 페이지 열어주는 메서드
+	@GetMapping("/regist")
+	public String regist() {
+		return "freeboard/freeRegist";
+	}
+	
 	//글 등록 처리
-	@PostMapping("/freeRegist")
-	public void regist(FreeBoardVO vo) {
+	@PostMapping("/regist")
+	public String regist(FreeBoardVO vo) {
 		service.regist(vo);
+		return "redirect:/freeboard/freeList";
 	}
 	
 	//글 상세 보기 처리
-	@GetMapping("/content")
-	public String freeContent(int bno, Model model) {
+	@GetMapping("/content/{bno}")
+	public String freeContent(@PathVariable("bno") int bno, Model model) {
 		model.addAttribute("article", service.getContent(bno));
 		return "freeboard/freeDetail";
 	}
 	
-	//글 수정 처리
+	//글 수정 페이지 이동 처리
 	@PostMapping("/modify")
-	public String modify(FreeBoardVO vo) {
+	public String modify(@ModelAttribute("article") FreeBoardVO vo) {
+		return "freeboard/freeModify";
+	}
+	
+	//글 수정 처리
+	@PostMapping("/update")
+	public String update(FreeBoardVO vo) {
 		service.update(vo);
-		return "redirect:/freeboard/content?bno=" + vo.getBno();
+		return "redirect:/freeboard/content/" + vo.getBno();
 	}
 	
 	@PostMapping("/delete")
