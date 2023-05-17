88071a443031c8fbbde5aca417164e9050c78a54
teataeho <rlaxogh9504@gmail.com>
2023-05-17 14:36:19 +0900

    2023.05.17 페이징

--- MyWeb/src/main/java/com/spring/myweb/controller/FreeBoardController.java
+++ MyWeb/src/main/java/com/spring/myweb/controller/FreeBoardController.java
@@ -1,4 +1,4 @@
-package com.spring.myweb;
+package com.spring.myweb.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring.myweb.command.FreeBoardVO;
 import com.spring.myweb.freeboard.service.IFreeBoardService;
+import com.spring.myweb.util.PageCreator;
+import com.spring.myweb.util.PageVO;
 
 @Controller
 @RequestMapping("/freeboard")
@@ -21,8 +23,12 @@ public class FreeBoardController {
 	
 	//목록 화면
 	@GetMapping("/freeList")
-	public void freeList(Model model) {
-		model.addAttribute("boardList", service.getList());
+	public void freeList(PageVO vo, Model model) {
+		
+		PageCreator pc = new PageCreator(vo, service.getTotal());
+		System.out.println(pc.toString());
+		model.addAttribute("boardList", service.getList(vo));
+		model.addAttribute("pc", pc);
 	}
 	
 	//글쓰기 페이지 열어주는 메서드
@@ -39,8 +45,18 @@ public class FreeBoardController {
 	}
 	
 	//글 상세 보기 처리
+	
+	/*
+	 @PathVariable은 URL 경로에 변수를 포함시켜 주는 방식
+	 null이나 공백이 들어갈 수 있는 파라미터라면 적용하지 않는 것을 추천
+	 파라미터 값에 .이 포함되어 있다면 .뒤의 값은 잘린다는 것을 알아두세요.
+	 {}안에 변수명을 지어주시고, @PathVariable 괄호 안에 영역을 지목해서
+	 값을 받아옵니다.
+	 */
 	@GetMapping("/content/{bno}")
-	public String freeContent(@PathVariable("bno") int bno, Model model) {
+	public String freeContent(@PathVariable int bno
+			, @ModelAttribute("p") PageVO vo
+			, Model model) {
 		model.addAttribute("article", service.getContent(bno));
 		return "freeboard/freeDetail";
 	}
